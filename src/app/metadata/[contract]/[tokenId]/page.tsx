3a889c530e57c4895e03c772198405a2df4c500f
tmoindustries <morgantg@gmail.com>
2024-12-02 21:39:38 -0700

    add CSP and sandbox compatibility for animation_url viewer

--- src/app/metadata/[contract]/[tokenId]/page.tsx
+++ src/app/metadata/[contract]/[tokenId]/page.tsx
@@ -3,5 +3,9 @@ import { ModuleView } from '@/modules/metadata/ModuleView';
 export default function MetadataViewPage({ params }: { 
   params: { contract: string; tokenId: string } 
 }) {
-  return <ModuleView contract={params.contract} tokenId={params.tokenId} />;
+  return (
+    <div data-sandbox-compatible="true">
+      <ModuleView contract={params.contract} tokenId={params.tokenId} />
+    </div>
+  );
 }
